b722bdc4bd42c46739250c474b767eb381c29fda
Benjamin Surdilla <benjsurdilla@gmail.com>
2021-11-17 14:12:25 +0800

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import React from 'react';
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-);
-
-// const HomePage = (props) => {
-//   console.log(props);
-//   return (
-//     <div>
-//       <button onClick={() =>  props.history.push('/topics')}>Topics</button>
-//       <h1>HOME PAGE</h1>
-//     </div>
-//   );
-// }
-
-// const TopicList = (props) => {
-//   return (
-//     <div>
+import ShopPage from './pages/shop/shop.component.jsx';
 
 
-//       <h1>TOPIC LIST PAGE</h1>
-//       <Link to={`${props.match.url}` + 13}>TO TOPIC 13</Link> &nbsp;
-//       <Link to={`${props.match.url}` + 15}>TO TOPIC 14</Link> &nbsp;
-//       <Link to={`${props.match.url}` + 15}>TO TOPIC 15</Link> &nbsp;
-//     </div>
-//   );
-// }
-
-// const TopicDetail = (props) => {
-//   console.log(props);
-//   return (
-//     <div>
-//       {/*     */}
-//       <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-//     </div>
-//   );
-// }
 const App = () =>
 (
   <div>
-    <Route exact path="/" component={HomePage} />
-    <Route exact path="/hats" component={HatsPage} />
-    {/* <Route exact path="/blog/asdqw/topics" component={TopicList} />
-    <Route path="/blog/asdqw/topics/:topicId" component={TopicDetail} />
-    <Route exact path="/blog/topics" component={TopicList} />
-    <Route path="/blog/topics/:topicId" component={TopicDetail} /> */}
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={ShopPage} />
+    </Switch>
   </div>
 );
 
