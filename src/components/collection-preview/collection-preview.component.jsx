5117f921e165a7fc69f394971056f5527fd96fb8
Benjamin Surdilla <benjsurdilla@gmail.com>
2021-12-05 21:35:23 +0800

    moved our directory and shop data into reducer , created respective selector and updated corresponding components with new ux flow for directory/shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,18 +4,21 @@ import CollectionItem from "../collection-item/collection-item.component";
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, idx}) => {
+    console.log(title)
+    console.log(items)
+    return(
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items
-                .filter((item, idx) => idx < 4)
+                .filter((item, idx) => idx < 6)
                 .map((item) => (
                     <CollectionItem key={item.id} item={item} />
-                ))}
+            ))}
         </div>
     </div>
 
-);
+)};
 
 export default CollectionPreview;
